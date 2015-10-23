dd472926a587d538189948a91f95d2817d6df4de
Xavier Gomez Ponce <xgp3@msn.com>
2015-10-23 11:59:57 +0200

    Change to Atmel Studio v7

---
+++ Marlin/__vm/.Marlin.vsarduino.h
@@ -0,0 +1,88 @@
+/* 
+	Editor: http://www.visualmicro.com
+	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: Arduino Mega w/ ATmega2560 (Mega 2560), Platform=avr, Package=arduino
+*/
+
+#define __AVR_ATmega2560__
+#define ARDUINO 164
+#define ARDUINO_MAIN
+#define F_CPU 16000000L
+#define __AVR__
+#define F_CPU 16000000L
+#define ARDUINO 164
+#define ARDUINO_AVR_MEGA2560
+#define ARDUINO_ARCH_AVR
+extern "C" void __cxa_pure_virtual() {;}
+
+
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\variants\mega\pins_arduino.h" 
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\cores\arduino\arduino.h"
+#include <Marlin.pde>
+#include <BlinkM.cpp>
+#include <BlinkM.h>
+#include <Configuration.h>
+#include <ConfigurationStore.cpp>
+#include <ConfigurationStore.h>
+#include <Configuration_adv.h>
+#include <DOGMbitmaps.h>
+#include <Hysteresis.cpp>
+#include <Hysteresis.h>
+#include <LCD_Handler.h>
+#include <LiquidCrystalRus.cpp>
+#include <LiquidCrystalRus.h>
+#include <Marlin.h>
+#include <Marlin.ino>
+#include <Marlin_main.cpp>
+#include <Sd2Card.cpp>
+#include <Sd2Card.h>
+#include <Sd2PinMap.h>
+#include <SdBaseFile.cpp>
+#include <SdBaseFile.h>
+#include <SdFatConfig.h>
+#include <SdFatStructs.h>
+#include <SdFatUtil.cpp>
+#include <SdFatUtil.h>
+#include <SdFile.cpp>
+#include <SdFile.h>
+#include <SdInfo.h>
+#include <SdVolume.cpp>
+#include <SdVolume.h>
+#include <Servo.cpp>
+#include <Servo.h>
+#include <Touch_Screen_Definitions.h>
+#include <cardreader.cpp>
+#include <cardreader.h>
+#include <digipot_mcp4451.cpp>
+#include <dogm_font_data_marlin.h>
+#include <dogm_lcd_implementation.h>
+#include <fastio.h>
+#include <genieArduino.cpp>
+#include <genieArduino.h>
+#include <language.cpp>
+#include <language.h>
+#include <motion_control.cpp>
+#include <motion_control.h>
+#include <pins.h>
+#include <planner.cpp>
+#include <planner.h>
+#include <qr_solve.cpp>
+#include <qr_solve.h>
+#include <speed_lookuptable.h>
+#include <stepper.cpp>
+#include <stepper.h>
+#include <temperature.cpp>
+#include <temperature.h>
+#include <thermistortables.h>
+#include <ultralcd.cpp>
+#include <ultralcd.h>
+#include <ultralcd_implementation_hitachi_HD44780.h>
+#include <ultralcd_st7920_u8glib_rrd.h>
+#include <vector_3.cpp>
+#include <vector_3.h>
+#include <watchdog.cpp>
+#include <watchdog.h>

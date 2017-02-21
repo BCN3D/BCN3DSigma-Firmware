69cdd09f6560329205b4ab8c7f1099e59f612949
AlejandroGarcia92 <agarciamoreno@fundaciocim.org>
2017-02-21 13:11:59 +0100

    speed x

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -844,6 +844,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
 #define NUM_LINES 10
 //#define HOMING_FEEDRATE {50*60, 50*60, 4*60, 0}  // set the homing speeds (mm/min)
+#define HOMING_FEEDRATE {60*60, 90*60, 15*60, 0} ///{75*60, 65*60, 6*60, 0}  // old homing speeds (mm/min)
 #define CALIB_FEEDRATE_ZAXIS 6*60 // bed homing speeds (mm/min)
 // default settings if screen not defined
 #ifndef SIGMA_TOUCH_SCREEN

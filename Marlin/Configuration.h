5638fdb169c0b9699c6660fcaa3b454f1dad2621
AlejandroGarcia92 <agarciamoreno@fundaciocim.org>
2017-02-24 12:49:12 +0100

    version 1.2.2_devL

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -27,8 +27,8 @@
 //This is the version declaration for Sigma, v followed by '-' first indicate the hardware, it must have 2 ditgits. Then the '-' and then the firmware, it has to have 3 digits separets by '.'. -> This is useful to
 //get the hw and fw version to Cura-BCN3D and update the new firmware
 
-#define VERSION_STRING  "01-1.2.2"
-//#define BUILD_DATE  "|M02.22"
+#define VERSION_STRING  "01-1.2.2_devL"
+#define BUILD_DATE  "|M02.27"
 #define VERSION_NUMBER  122
 #define BUILD_DATE  " "
 #define UI_SerialID  "At Bottom Sticker"

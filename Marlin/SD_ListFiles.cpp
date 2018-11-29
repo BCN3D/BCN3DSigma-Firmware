2bab6c3133ccc7b79d40119360a3d9ebcfe29fdf
AlejandroGarcia92 <agarciamoreno@fundaciocim.org>
2018-11-29 09:21:07 +0100

    fix parser

--- Marlin/SD_ListFiles.cpp
+++ Marlin/SD_ListFiles.cpp
@@ -378,11 +378,21 @@ float Listfiles::extract_ensure_hotend_gcode_setup_t(uint8_t tool){
 	int nozzle_digit3 =-1;
 	int nozzle_digit4 =-1;
 	float res=0.0;
-	if(simplify3D) sscanf_P( commandline, PSTR(";   extruderDiameter,%d.%d,%d.%d"), &nozzle_digit1, &nozzle_digit2, &nozzle_digit3, &nozzle_digit4);
-	else sscanf_P( commandline, PSTR(";Extruders used: T%d %d.%d T%d %d.%d"), &nozzle_0, &nozzle_digit1, &nozzle_digit2, &nozzle_1, &nozzle_digit3, &nozzle_digit4);
+	if(simplify3D){
+		if(tool == LEFT_EXTRUDER){
+			sscanf_P( commandline, PSTR(";   extruderDiameter,%d.%d"), &nozzle_digit1, &nozzle_digit2);
+		}else{
+			sscanf_P( commandline, PSTR(";   extruderDiameter,%d.%d,%d.%d"), &nozzle_digit1, &nozzle_digit2, &nozzle_digit3, &nozzle_digit4);
+			if(nozzle_digit1 == 1) sscanf_P( commandline, PSTR(";   extruderDiameter,1,%d.%d"), &nozzle_digit3, &nozzle_digit4);
+		}
+		
+	}
+	else{
+		sscanf_P( commandline, PSTR(";Extruders used: T%d %d.%d T%d %d.%d"), &nozzle_0, &nozzle_digit1, &nozzle_digit2, &nozzle_1, &nozzle_digit3, &nozzle_digit4);
+	}
 	if(nozzle_0 == 1){
-		nozzle_digit1 = -1;
-		nozzle_digit2 = -1;
+		nozzle_digit3 = nozzle_digit1;
+		nozzle_digit4 = nozzle_digit2;
 	}
 	if(nozzle_1 == 0){
 		nozzle_digit1 = nozzle_digit3;
